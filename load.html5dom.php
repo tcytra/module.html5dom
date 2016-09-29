1179ce62c51032fd3c323a450278cec4e453108c
Todd Cytra <tcytra@gmail.com>
2016-09-28 21:13:15 -0700

    Added the html5dom class loader file

---
+++ load.html5dom.php
@@ -0,0 +1,8 @@
+<?php
+require_once "./classes/class.html5dom.php";
+require_once "./classes/class.html5dom.construct.php";
+require_once "./classes/class.html5dom.document.php";
+require_once "./classes/class.html5dom.fragment.php";
+require_once "./classes/class.html5dom.element.php";
+require_once "./classes/class.html5dom.element.list.php";
+?>
