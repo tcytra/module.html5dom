b40dfed0f849e92323ee7cbbc172c6273a5402d7
Todd Cytra <tcytra@gmail.com>
2016-09-29 19:23:15 -0700

    Minor Update: Added a constructor to the HTML5Document object

--- classes/class.html5dom.document.php
+++ classes/class.html5dom.document.php
@@ -2,14 +2,31 @@
 /**
  *  Class	HTML5Document
  *  
- *  This class serves as the master handler for the DOMDocument in the HTML5Dom system
+ *  This Model is the top-level object of the HTML5Dom module for manipulating and extending
+ *  + the PHP DOMDocument system
  *  
  *  @author		Todd Cytra <tcytra@gmail.com>
- *  @version	0.2.3 class.html5.document.php 2016-09-14
+ *  @version	0.2.3 class.html5dom.document.php 2016-09-14
  *  @since		system-0.3.1
  */
 class HTML5Document
 {
+	private $charset;
+	private $language;
+	
+	private	$domdtd;
+	private	$domimp;
+	private	$domobj;
+	
+	/**
+	 * __construct()
+	 *  Create an instance of the HTML5Document object
+	 */
+	function __construct()
+	{
+		
+	}
+	
 	
 }
 ?>
