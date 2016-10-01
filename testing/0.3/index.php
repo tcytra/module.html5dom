bd440548f42c7e7c37f55868dd10cc8ff4303b9d
Todd Cytra <tcytra@gmail.com>
2016-09-30 20:25:40 -0700

    Update: Prepared a basic directory index page for developer notes

--- testing/0.3/index.php
+++ testing/0.3/index.php
@@ -1,7 +1,28 @@
-<?php header("Content-Type:text/plain"); ?>
-<?php require "../load.html5dom.php"; ?>
-<?php
-
-
-
-?>
+<!DOCTYPE html>
+<html lang="en-CA">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width,initial-scale=1"/>
+		<title>0.3 | HTML5Dom Testing</title>
+	</head>
+	<body>
+		<div class="page interface">
+			<header class="head">
+				<h1 class="heading">HTML5Dom Testing</h1>
+			</header>
+			<div class="body">
+				<main>
+					<section class="description">
+						<h2>0.3</h2>
+						<p>Create an instance of the PHP DOMDocument via the DOMImplementation</p>
+					</section>
+					<section class="objectives">
+						<h2>Tests</h2>
+						<p></p>
+						<code></code>
+					</section>
+				</main>
+			</div>
+		</div>
+	</body>
+</html>
