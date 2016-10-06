196458ef260bc58566cfb58bc3ef17e494058013
Todd Cytra <tcytra@gmail.com>
2016-10-05 19:41:01 -0700

    File Update: Altered the classes filesystem/file names and object names

--- load.html5dom.php
+++ load.html5dom.php
@@ -1,8 +1,8 @@
 <?php
-require_once "classes/class.html5dom.php";
-require_once "classes/class.html5dom.construct.php";
-require_once "classes/class.html5dom.document.php";
-require_once "classes/class.html5dom.fragment.php";
-require_once "classes/class.html5dom.element.php";
-require_once "classes/class.html5dom.element.list.php";
+require_once "Module/HTML5Dom.php";
+require_once "Module/HTML5Construct.php";
+require_once "Module/HTML5Document.php";
+require_once "Module/HTML5Fragment.php";
+require_once "Module/HTML5Element.php";
+require_once "Module/HTML5ElementList.php";
 ?>
--- testing/0.3/test-03.php
+++ testing/0.3/test-03.php
@@ -1,5 +1,12 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
 <?php
 
+//  Test 02
+//  Create Document with html and body nodes via instance model
 
+$html5 = new HTML5Document("html");
+
+$html5->save()->write();
 
 ?>
