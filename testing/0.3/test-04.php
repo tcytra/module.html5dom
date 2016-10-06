b75766f1ed02a75aecb51082da174143ed5dc8ee
Todd Cytra <tcytra@gmail.com>
2016-10-05 21:02:58 -0700

    Testing: Test 04 passed; Boolean arguments for "html" and "body" nodes
    The implement method will also accept the values (true, true) from the
    object constructor

---
+++ testing/0.3/test-04.php
@@ -0,0 +1,13 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Test 03
+//  Create Document with html and body nodes via boolen arguments to the
+//  + instance constructor. Result should be the same as test-03
+
+$html5 = new HTML5Document(true, true);
+
+$html5->save()->write();
+
+?>
