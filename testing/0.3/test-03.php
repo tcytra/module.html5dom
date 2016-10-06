8d2ebd11d1603cf3381c43eaaaa3693e5b8ed36e
Todd Cytra <tcytra@gmail.com>
2016-10-05 20:57:08 -0700

    Testing: Test 03 passed; Ability to argue for "html" and "body" nodes
    The implement method will accept the values ("html", "body") from the
    object constructor
    
    The object target node will cascade from "html" to "body" as they exist

--- testing/0.3/test-03.php
+++ testing/0.3/test-03.php
@@ -2,10 +2,10 @@
 <?php require "../../load.html5dom.php"; ?>
 <?php
 
-//  Test 02
+//  Test 03
 //  Create Document with html and body nodes via instance model
 
-$html5 = new HTML5Document("html");
+$html5 = new HTML5Document("html","body");
 
 $html5->save()->write();
 
