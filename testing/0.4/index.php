c67c9edabc9c7a636794b1ecfddbc0ae0fb7a31f
Todd Cytra <tcytra@gmail.com>
2016-10-06 20:13:48 -0700

    File Update: Created the 0.4 directory index and test handler

---
+++ testing/0.4/index.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<article id="test-0-4">
+	<header class="header">
+		<h2>0.4</h2>
+		<p>Create an instance of the PHP DOMDocument via the HTML5Dom static methods</p>
+	</header>
+	<section class="tests">
+		
+	</section>
+</article>
---
+++ testing/0.4/test-01.php
@@ -0,0 +1,10 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Test 01
+//  Create full HTML5 Document via HTML5Dom static model
+
+HTML5Dom::CreateDocument();
+
+?>
