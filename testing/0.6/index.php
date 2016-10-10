cb2ac9f5893440928a19c168a737cc98045022c7
Todd Cytra <tcytra@gmail.com>
2016-10-10 09:07:40 -0700

    File Update: Created the testing/0.6 directory index and test-01 handler

---
+++ testing/0.6/index.php
@@ -0,0 +1,16 @@
+<article id="test-0-6">
+	<header class="header">
+		<h2>0.6</h2>
+		<p>Require the ability to create, identify, and manipulate the head and body elements distinctly</p>
+	</header>
+	<section class="tests">
+		
+	</section>
+	<section class="notes">
+		<h2>Notes</h2>
+		<ul>
+			<li>Introduce the concept of a constructor to harmonize the Html5Document and Html5Element objects</li>
+			<li>Create public methods to directly access the Html5Document html, head, and body Html5Elements</li>
+		</ul>
+	</section>
+</article>
---
+++ testing/0.6/test-01.php
@@ -0,0 +1,12 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Test 01
+//  Create the Html5Document head and body as Html5Element objects
+
+$html5 = new HTML5Document("html", "body");
+
+
+
+$html5->save()->write();
