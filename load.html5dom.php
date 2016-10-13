7ef504e9ea1e85505e61f732c4db0cb0c28f9796
Todd Cytra <tcytra@gmail.com>
2016-10-12 19:58:05 -0700

    File Update: Added the HTML5DocumentHead to the html5dom load script

--- load.html5dom.php
+++ load.html5dom.php
@@ -2,6 +2,7 @@
 require_once "Module/HTML5Dom.php";
 require_once "Module/HTML5Construct.php";
 require_once "Module/HTML5Document.php";
+require_once "Module/HTML5DocumentHead.php";
 require_once "Module/HTML5Fragment.php";
 require_once "Module/HTML5Element.php";
 require_once "Module/HTML5ElementList.php";
