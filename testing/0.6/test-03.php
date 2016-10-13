17731c495c692b9b71fe3b9d78e71faeb74ec2e3
Todd Cytra <tcytra@gmail.com>
2016-10-12 20:18:42 -0700

    Testing: 0.6-03 passed; Able to reproduce 0.6-02; Able to chain methods

---
+++ testing/0.6/test-03.php
@@ -0,0 +1,13 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Test 03
+//  Move the head specific methods to the HTML5DocumentHead object
+
+$html5 = new HTML5Document("html", "body");
+$html5->meta( ["viewport"=>"width=device-width,initial-scale=1"] )->title("0.6 Test 02");
+
+$html5->append("div")->attribute("id", "homepage")->attribute("class", "page interface");
+
+$html5->save()->write();
