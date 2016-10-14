aaddd2f6da154a6c561c3dcf7ff8ff5df3e8ff2c
Todd Cytra <tcytra@gmail.com>
2016-10-13 22:28:39 -0700

    Minor Update: Cleanup on testing/0.6/index.php

--- testing/0.6/index.php
+++ testing/0.6/index.php
@@ -1,7 +1,7 @@
 <article id="test-0-6">
 	<header class="header">
 		<h2>0.6</h2>
-		<p>Require the ability to create, identify, and manipulate the head and body elements distinctly</p>
+		<p>Require the ability to create, identify, and manipulate the &lt;head&gt; elements distinctly</p>
 	</header>
 	<section class="tests">
 		
@@ -9,7 +9,6 @@
 	<section class="notes">
 		<h2>Notes</h2>
 		<ul>
-			<li>Introduce the concept of a constructor to harmonize the Html5Document and Html5Element objects</li>
 			<li>Create public methods to directly access the Html5Document html, head, and body Html5Elements</li>
 		</ul>
 	</section>
