9f1d1d0a1a1c6416d38d46f26206c571ce7d0c00
Todd Cytra <tcytra@gmail.com>
2016-10-13 20:13:20 -0700

    Testing: 0.6-05 passed; Call meta after title have properly ordered tags

---
+++ testing/0.6/test-05.php
@@ -0,0 +1,19 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Test 05
+//  Ensure the meta tags are appended before the title tag
+
+$html5 = new HTML5Document("html", "body");
+
+$html5->title("0.6");
+$html5->title("Test 04", 1, " ");
+$html5->title("HTML5Dom", -1, "|");
+
+$html5->meta( ["viewport"=>"width=device-width,initial-scale=1"] );
+$html5->meta( ["name"=>"author","content"=>"Todd Cytra tcytra.gmail.com."] );
+
+$html5->append("div")->attribute("id", "homepage")->attribute("class", "page interface");
+
+$html5->save()->write();
