5a6c3d25a392e89b295dea7ee4fa46c2e7fc31a5
Todd Cytra <tcytra@gmail.com>
2016-10-13 22:33:02 -0700

    Testing: 0.6-06 passed; Able to create link tags in the document head

--- testing/0.6/test-06.php
+++ testing/0.6/test-06.php
@@ -14,12 +14,12 @@ $html5->head->title("0.6")
 	->title("HTML5Dom", -1, "|");
 
 //  Create some document <meta> tags
-$html5->head->meta( ["viewport"=>"width=device-width,initial-scale=1"] );
-$html5->meta( ["name"=>"author","content"=>"Todd Cytra tcytra.gmail.com."] );
-$html5->meta( ["name"=>"description","content"=>$describe] );
+$html5->head->meta( ["viewport"=>"width=device-width,initial-scale=1"] )
+	->meta( ["name"=>"author","content"=>"Todd Cytra tcytra.gmail.com."] )
+	->meta( ["name"=>"description","content"=>$describe] );
 
-//  Create a document stylesheet <link>
-$html5->head->link("include/default.css");
+//  Create some document stylesheet <link>
+$html5->head->link("include/default.css")->link("include/utility.css");
 
 //  Create a document <body> element
 $html5->append("div")->attribute("id", "homepage")->attribute("class", "page interface");
