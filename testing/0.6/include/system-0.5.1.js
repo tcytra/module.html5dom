28855f74c72b5c4608e598ed7686319b525a3932
Todd Cytra <tcytra@gmail.com>
2016-10-16 13:20:25 -0700

    File Update: Added test-07.php and include/system-0.5.1.js for testing

---
+++ testing/0.6/include/system-0.5.1.js
@@ -0,0 +1,9 @@
+/*  author:  Todd Cytra <tcytra@gmail.com
+ *  version: system-0.5.1.js 2016-10-16
+ */
+
+(function(){
+	
+	document.onload = function(){ console.log("Here we are then."); };
+	
+})();
