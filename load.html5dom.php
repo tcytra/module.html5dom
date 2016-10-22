56f6ec7dc056fbe58492af0bbf97cb76480eab87
Todd Cytra <tcytra@gmail.com>
2016-10-21 21:14:06 -0700

    Update: Edited the load.html5dom.php file to require the correct files

--- load.html5dom.php
+++ load.html5dom.php
@@ -1,9 +1,12 @@
 <?php
-require_once "Module/HTML5Dom.php";
-require_once "Module/HTML5Construct.php";
-require_once "Module/HTML5Document.php";
-require_once "Module/HTML5DocumentHead.php";
-require_once "Module/HTML5Fragment.php";
-require_once "Module/HTML5Element.php";
-require_once "Module/HTML5ElementList.php";
+//  the global object
+require_once "Module/Html5Dom.php";
+//  the instance system
+require_once "Module/Html5.php";
+require_once "Module/Html5Construct.php";
+require_once "Module/Html5Document.php";
+require_once "Module/Html5DocumentHead.php";
+require_once "Module/Html5Fragment.php";
+require_once "Module/Html5Element.php";
+require_once "Module/Html5ElementList.php";
 ?>
