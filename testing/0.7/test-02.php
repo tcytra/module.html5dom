092b6aeafb3e02a36742564282fb032fc43b8944
Todd Cytra <tcytra@gmail.com>
2016-10-21 21:34:34 -0700

    Testing: 0.7-02 passed; Able to reproduce test-01 results

---
+++ testing/0.7/test-02.php
@@ -0,0 +1,28 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Test 01
+//  Create a basic parent Html5 object for this system
+
+$html5 = new HTML5Document("html", "body");
+
+//  Title and describe this testcase
+$testcase = "Test 01";
+$describe = "Require the ability to add a favicon link element to the document head";
+
+//  Assemble some javascript code to include with the <script> element
+$code = <<<JavaScript
+(function(){\nwindow.onload = function(){\n\tdocument.getElementById("testing").innerHTML = "<h1>{$testcase}</h1><p>{$describe}.</p>";\n}\n})();
+JavaScript;
+
+//  Create the document <head> elements
+$html5->head->meta( ["name"=>"description","content"=>$describe] )
+	->title("HTML5Dom | 0.7 {$testcase}")
+	->javascript(null, $code)
+	->favicon();
+
+//  Create a document <body> element
+$html5->append("div")->attribute("id", "testing")->attribute("class", "page interface");
+
+$html5->save()->write();
