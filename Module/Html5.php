ae84b640e2b5239c5f387d0ea18c0cbc63f2d27d
Todd Cytra <tcytra@gmail.com>
2016-10-21 20:54:54 -0700

    File Update: Added the Html5.php script with the new parent Html5 object

---
+++ Module/Html5.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *  Html5
+ *  
+ *  This object is the parent instance in the Html5 object model and contains
+ *  + the top level configuration and dom references and interaction
+ *  
+ *  @author     Todd Cytra <tcytra@gmail.com>
+ *  @version    0.1.1 HTML5Document.php 2016-10-21
+ *  @since      Html5-0.0.7
+ */
+class Html5
+{
+	//  Html5 Document Parameters
+	
+	/** @var string $charset  Is the specified character set for this HTML5 output */
+	public	static	$charset	= "utf-8";
+	/** @var string $language Is the specified language encoding for this HTML5 output */
+	public	static	$language	= "en-US";
+	
+	/**
+	 * __construct()
+	 *  Create an instance of the Html5 object
+	 *  
+	 *  @param  object  $config = null
+	 */
+	public	function __construct($config = null)
+	{
+		
+	}
+	
+	/**
+	 *  configure()
+	 *  Set a configuration value for this object by specified index
+	 *  
+	 *  @param  string  $index
+	 *  @param  string  $value
+	 *  @access private
+	 */
+	private	function configure($index, $value)
+	{
+		switch ($index) {
+			case 'charset':
+				if (preg_match("/^[a-z][a-z0-9-]+[0-9]{1}$/", $value)) { self::$charset = $value; }
+				break;
+		}
+	}
+}
