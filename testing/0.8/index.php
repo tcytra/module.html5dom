e85ebca4ee4afbdb9239c833c5e1c3bc96d6a863
Todd Cytra <tcytra@gmail.com>
2016-10-23 10:41:54 -0700

    File Update: Created the testing/0.8 directory index and test-01 handler

---
+++ testing/0.8/index.php
@@ -0,0 +1,16 @@
+<article id="test-0-8">
+	<header class="header">
+		<h2>0.8</h2>
+		<p>Provide the ability to create and identify DomDocument objects via a standard contructor argument</p>
+	</header>
+	<section class="tests">
+		
+	</section>
+	<section class="notes">
+		<h2>Notes</h2>
+		<ul>
+			<li>Call to the append method with a node#id.classname construct and repeat current behavior</li>
+			<li>Provide the ability to argue for the construct with additional instructions, ie. content</li>
+		</ul>
+	</section>
+</article>
---
+++ testing/0.8/test-01.php
@@ -0,0 +1,26 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Title and describe this testcase
+$testcase = "Test 01";
+$describe = "Move the append method from the Html5Document to the Html5 object";
+
+//  Create an instance of the Html5Document
+$html5 = new Html5Document(['language'=>'en_CA']);
+
+//  Assemble some javascript code to include with the <script> element
+$code = <<<JavaScript
+(function(){\nwindow.onload = function(){\n\tdocument.getElementById("testing").innerHTML = "<h1>{$testcase}</h1><p>{$describe}.</p>";\n}\n})();
+JavaScript;
+
+//  Create the document <head> elements
+$html5->head->meta( ["name"=>"description","content"=>$describe] )
+	->title("Html5Dom | 0.8 {$testcase}")
+	->javascript(null, $code)
+	->favicon("/favicon.png");
+
+//  Create a document <body> element
+$html5->append("div")->attribute("id", "testing")->attribute("class", "page interface");
+
+$html5->save()->write();
