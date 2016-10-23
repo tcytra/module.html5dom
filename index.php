fd5dfa35e3afa0404910b7a85c04e14a1d94e736
Todd Cytra <tcytra@gmail.com>
2016-10-23 10:13:32 -0700

    Note: Updated the module index.php notes

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 	<body>
 		<div class="page interface">
 			<header class="head">
-				<h1 class="heading">HTML5Dom</h1>
+				<h1 class="heading">Html5Dom</h1>
 			</header>
 			<div class="body">
 				<main>
 					<section class="description">
 						<h2>Description</h2>
-						<p>The HTML5Dom system is intended to be an interface for HTML5 dom-node creation and manipulation via string-format nodename and element id selectors.</p>
+						<p>The Html5 system is intended to be an interface for HTML5 dom-node creation and manipulation via string-format nodename and element id selectors.</p>
 					</section>
 					<section class="objectives">
 						<h2>Objectives</h2>
@@ -23,19 +23,32 @@
 							<li>Top-down HTML5 rendering: The ability to define an HTML5 document and build the internal structure within it.</li>
 							<li>Bottom-up HTML5 rendering: The ability to define an HTML5 fragment and build the remaining document upwards around it.</li>
 							<li>Detached simultaneous fragments: The ability to have several HTML5 fragments detached from the document structure indexed and accessible for further work.</li>
+							<li>Chain and/or repeat element manipulation methods to create HTML5 node treet structures and populate with externally derived content.</li>
 						</ul>
 					</section>
 					<section class="requirements">
 						<h2>Requirements</h2>
 						<ul>
 							<li>Require the ability to append an element with an argument for content</li>
-							<li>Require an isvalid object to test element names and attributes, document title, etc</li>
+							<li>
+								<strike>Require an isvalid object to test element names and attributes, document title, etc</strike>
+								<ul>
+									<li>isValid has been added as a static method to the Html5 object</li>
+								</ul>
+							</li>
 							<li>Shorten the element append and attributes "id" and "class" to a constructor argument</li>
-							<li>Argue the charset and language to the HTML5Document __construct and call implement with node tags separately</li>
+							<li>
+								<strike>Argue the charset and language to the Html5Document __construct and call implement with node tags separately</strike>
+								<ul>
+									<li>The charset and language arguments are passed through the $config to the Html5 constructor</li>
+									<li>The implement method is still automatic, but could be renamed as the create method, which is consistent with sibling objects</li>
+								</ul>
+							</li>
 							<li>Require the ability to create, manipulate, and output a PHP DomFragment</li>
 							<li>Require the ability to populate an Html5 Document, Fragment, or Element from a text or data source</li>
 							<li>Require the ability to specify environment behavior through a configuration system (Which could chain up a parent system)</li>
 							<li>Utilize the Html5 namespace to contain this module</li>
+							<li>Consider the possibility of introducing an Html5DocumentBody object to manage the concept of interface pages, dialogues, and toolbars</li>
 						</ul>
 					</section>
 				</main>
