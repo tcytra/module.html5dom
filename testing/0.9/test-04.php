2ac1895cfc0c9e330a0e4f448ee271aea0ac721a
Todd Cytra <tcytra@gmail.com>
2016-10-28 22:05:41 -0700

    Testing: 0.9-04 passed; Able to reproduce 0.9-03 via standalone fragment

---
+++ testing/0.9/test-04.php
@@ -0,0 +1,16 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Title and describe this testcase
+$testcase = "Test 04";
+$describe = "Ensure the ability to wrap a stand-alone Html5Fragment with a constructor argument";
+
+//  Create an instance of the Html5Document
+$fragment = new Html5Fragment();
+$fragment->create("header.testcase", "<h3>{$testcase}</h3><p>{$describe}</p>");
+
+//  Wrap the fragment with a specified construct
+$fragment->wrap("#interface");
+
+$fragment->save()->write();
