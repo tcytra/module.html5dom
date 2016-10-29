0a954a52339eebc1539ddb37304f5ad5198d7f10
Todd Cytra <tcytra@gmail.com>
2016-10-28 21:59:08 -0700

    Testing: 0.9-03 passed; Able to wrap the fragment with a construct node

---
+++ testing/0.9/test-03.php
@@ -0,0 +1,21 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Title and describe this testcase
+$testcase = "Test 03";
+$describe = "Provide the ability to wrap an Html5Fragment with a constructor argument";
+
+//  Create an instance of the Html5Document
+$html5 = new Html5Document("en_CA");
+
+//  Create an instance of the Html5Fragment
+$fragment = $html5->fragment("header.testcase", "<h3>{$testcase}</h3><p>{$describe}</p>");
+
+//  Wrap the fragment with a specified construct
+$fragment->wrap("#interface");
+
+//  Append the fragment to the Html5Document <body>
+$fragment->appendTo($html5->body);
+
+$html5->save()->write();
