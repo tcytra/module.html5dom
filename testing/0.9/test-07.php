9817c00ad11863edb872e2e8f061f466629bdf98
Todd Cytra <tcytra@gmail.com>
2016-10-29 17:56:02 -0700

    Testing: 0.9-07 passed; Able to clone a fragment work with it separately

---
+++ testing/0.9/test-07.php
@@ -0,0 +1,33 @@
+<?php require "../../errors.php"; ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Title and describe this testcase
+$testcase = "Test 07";
+$describe = "Clone an Html5Fragment into a target node";
+
+//  Create an instance of the Html5Document
+$html5 = new Html5Document("en_CA");
+
+$webpage = <<<MainContent
+<header>
+	<h1 class="pagetitle"></h1>
+	<h2 class="description"></h2>
+</header>
+MainContent;
+
+//  Create an instance of the Html5Fragment
+$fragment = $html5->fragment("div.interface.wrapper", $webpage);
+
+//  Attempt to create copies of the Html5Fragment
+$fragment->cloneFragment()
+	->append("main.main.content", "This is the #overview clone")
+	->wrap("div#overview.page.wrapper")
+	->appendTo($html5->body);
+
+$fragment->cloneFragment()
+	->append("main.main.content", "This is the #versions clone")
+	->wrap("div#versions.page.wrapper")
+	->appendTo($html5->body);
+
+$html5->save()->write();
