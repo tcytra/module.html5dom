6acd1229daf6d03d221cffed1f9d8342b54a7645
Todd Cytra <tcytra@gmail.com>
2016-11-05 12:16:39 -0700

    Testing: 1.01-01 passed; Able to reproduce previous results

--- testing/1.01/test-01.php
+++ testing/1.01/test-01.php
@@ -7,10 +7,18 @@ $testcase = "Test 01";
 $describe = "Declare the Html5 object as abstract, move the config evaluation to the Html5Document";
 
 //  Create an instance of the Html5Document
-$html5 = new Html5Document();
+$html5 = new Html5Document( ["language"=>"en-CA", "charset"=>"utf-16"] );
+
+//  Append some elements to the document <head>
+$html5->head->title($testcase)
+	->meta(["name"=>"author", "content"=>"Todd Cytra tcytra.gmail.com."])
+	->stylesheet("/css/styles.css")
+	->favicon();
 
 //  Create an instance of the Html5Fragment
-$fragment = $html5->fragment("div#testing.interface","<h3>{$testcase}</h3><p>{$describe}</p>");
+$fragment = $html5->fragment("div#testing.interface", "<h3>{$testcase}</h3><p>{$describe}</p>");
+
+$fragment->append("p", "This is an appended element");
 
 //  Append the fragment to the Html5Document <body>
 $fragment->appendTo($html5->body);
