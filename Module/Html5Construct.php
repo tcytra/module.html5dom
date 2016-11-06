57b3a2efdb7b4d95c4e1a6aca7cddd81a2fd1a4c
Todd Cytra <tcytra@gmail.com>
2016-11-06 10:22:33 -0800

    Update: Renamed Html5Construct Set to Explode; added the evaluate method
    Also the object accepts a strict argument to prevent assuming a nodeName

--- Module/Html5Construct.php
+++ Module/Html5Construct.php
@@ -10,6 +10,8 @@
  */
 class Html5Construct
 {
+	static  $defaultNode = "div";
+	
 	//  Public Parameters
 	
 	/** @var string $id */
@@ -23,8 +25,44 @@ class Html5Construct
 	 * __construct()
 	 *  Create a new instance of the HTML5Construct
 	 */
-	function __construct()
-	{  }
+	function __construct($constructor, $strict = false)
+	{
+		//  if the constructor is an object, return the object
+		if (gettype($constructor) === "object") { return $constructor; }
+		
+		//  otherwise, evaluate the provided constructor
+		$this->evaluate($constructor, $strict);
+	}
+	
+	//  Private Methods
+	
+	/**
+	 *  evaluate()
+	 *  Evaluate a provided construct and break it down into logical components
+	 *  + If set true, strict will not allow the object to assume a defaultNode
+	 *  
+	 *  @param  string  $constructor
+	 *  @param  bool    $strict
+	 *  @access private
+	 *  @throws Exception
+	 */
+	private function evaluate($constructor, $strict)
+	{
+		//  extract a node class definition, if available; match '.classname'
+		if(preg_match("/\.[a-z]?([a-z0-9-]+)$/", $constructor))
+		{ $this->class = str_replace(".", " ", substr($constructor, strpos($constructor,".")+1)); $constructor = substr($constructor, 0, strpos($constructor,".")); }
+		
+		//  extract a node identifier, if available; match '#elementid'
+		if(preg_match("/#[a-z]?([a-zA-Z0-9_]+)$/", $constructor))
+		{ $this->id = substr($constructor, strpos($constructor,"#")+1); $constructor = substr($constructor, 0, strpos($constructor,"#")); }
+		
+		//  verify the remainder as the node name
+		$this->name = preg_match("/^[a-z]?([a-zA-Z0-9-]+)$/", $constructor) ? $constructor : (($strict) ? null : self::$defaultNode);
+		
+		//  ensure the construct node is a valid HTML5 entity
+		if(!$this->able())
+		{ throw new Exception("The construct node '{$this->name}' is not a valid HTML5 entity."); }
+	}
 	
 	//  Public Methods
 	
@@ -43,35 +81,17 @@ class Html5Construct
 	//  Global Methods
 	
 	/**
-	 *  Set()
+	 *  Explode()
 	 *  Explode and evaluate the portions of the provided constructor
 	 *  
 	 *  @param  string|object $constructor
 	 *  @return object
 	 *  @access public
 	 */
-	public static function Set($constructor)
+	public static function Explode($constructor)
 	{
-		//  if the constructor is an object, return the object
-		if (gettype($constructor) === "object") { return $constructor; }
-		
-		//  create an instance of the HTML5Construct object
-		$construct	= new Html5Construct();
-		
-		//  extract a node class definition, if available; match '.classname'
-		if(preg_match("/\.[a-z]?([a-z0-9-]+)$/", $constructor))
-		{ $construct->class = str_replace(".", " ", substr($constructor, strpos($constructor,".")+1)); $constructor = substr($constructor, 0, strpos($constructor,".")); }
-		
-		//  extract a node identifier, if available; match '#elementid'
-		if(preg_match("/#[a-z]?([a-zA-Z0-9_]+)$/", $constructor))
-		{ $construct->id = substr($constructor, strpos($constructor,"#")+1); $constructor = substr($constructor, 0, strpos($constructor,"#")); }
-		
-		//  verify the remainder as the node name
-		$construct->name = preg_match("/^[a-z]?([a-zA-Z0-9-]+)$/", $constructor) ? $constructor : "div";
-		
-		//  ensure the construct node is a valid HTML5 entity
-		if(!$construct->able())
-		{ throw new Exception("The construct node '{$construct->name}' is not a valid HTML5 entity."); }
+		//  create and return an instance of the HTML5Construct object
+		$construct	= new Html5Construct($constructor);
 		
 		return	$construct;
 	}
