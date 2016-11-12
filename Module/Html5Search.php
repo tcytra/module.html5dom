76dcc77639f4955159bfaae304b4fc77212e6ea8
Todd Cytra <tcytra@gmail.com>
2016-11-11 23:04:52 -0800

    Update: Added not method to Html5Search to exclude nodes from the list

--- Module/Html5Search.php
+++ Module/Html5Search.php
@@ -10,7 +10,7 @@
  *  + default as it may be the case a list of nodes match the search criteria
  *  
  *  @author		Todd Cytra <tcytra@gmail.com>
- *  @version	0.0.1 Html5Search.php 2016-11-06
+ *  @version	0.0.3 Html5Search.php 2016-11-06
  *  @since		html5-0.1.2
  */
 class Html5Search extends Html5
@@ -26,7 +26,7 @@ class Html5Search extends Html5
 	
 	/**
 	 * __construct()
-	 *  Create an instance of the Html5Document object
+	 *  Create an instance of the Html5Search object
 	 */
 	public function __construct($parent)
 	{
@@ -35,29 +35,28 @@ class Html5Search extends Html5
 	
 	//  Private Methods
 	
-	
 	/**
 	 *  findByClassName()
 	 *  Cycle through a node tree to find elements with the requested class attribute
 	 *  
-	 *  @param  string  $class
+	 *  @param  string  $className
 	 *  @param  object  $node
 	 *  @access private
 	 */
-	private function findByClassName($class, $node)
+	private function findByClassName($className, $node)
 	{
 		//  create an array of the classes to find
-		$find = explode(" ", $class);
+		$match = explode(" ", $className);
 		
 		//  only evaluate html tag nodes
 		if ($node->nodeType == 1) {
 			//  ensure the node has a class attribute
 			if ($node->hasAttribute("class")) {
 				//  convert the node classes into an array
-				$classes = explode(" ", $node->getAttribute("class"));
+				$class = explode(" ", $node->getAttribute("class"));
 				
 				//  compare the intersection of the class arrays
-				if (array_intersect($find, $classes) == $find) {
+				if (array_intersect($match, $class) == $match) {
 					//  add this node to the instance list
 					$this->list[] = $node;
 				}
@@ -66,7 +65,7 @@ class Html5Search extends Html5
 			//  continue searching through the available childNodes
 			if ($node->childNodes->length) {
 				//  cycle the childNodes back into this method
-				foreach ($node->childNodes as $each) { $this->findByClassName($class, $each); }
+				foreach ($node->childNodes as $each) { $this->findByClassName($className, $each); }
 			}
 		}
 	}
@@ -75,16 +74,16 @@ class Html5Search extends Html5
 	 *  findByNodeName()
 	 *  Cycle through a node tree to find elements with the requested nodeName
 	 *  
-	 *  @param  string  $name
+	 *  @param  string  $nodeName
 	 *  @param  object  $node
 	 *  @access private
 	 */
-	private function findByNodeName($name, $node)
+	private function findByNodeName($nodeName, $node)
 	{
 		//  only evaluate html tag nodes with class attributes
 		if ($node->nodeType == 1) {
 			//  compare the nodeName with the requested name
-			if (strtolower($node->nodeName) == strtolower($name)) {
+			if (strtolower($node->nodeName) == strtolower($nodeName)) {
 				//  add this node to the instance list
 				$this->list[] = $node;
 			}
@@ -92,7 +91,7 @@ class Html5Search extends Html5
 			//  continue searching through the available childNodes
 			if ($node->childNodes->length) {
 				//  cycle the childNodes back into this method
-				foreach ($node->childNodes as $each) { $this->findByNodeName($name, $each); }
+				foreach ($node->childNodes as $each) { $this->findByNodeName($nodeName, $each); }
 			}
 		}
 	}
@@ -101,30 +100,23 @@ class Html5Search extends Html5
 	
 	/**
 	 *  find()
-	 *  FInd matching nodes against a provided construct
+	 *  Find matching nodes against a provided construct
 	 *  
-	 *  @param  string  $construct
-	 *  @param  object  $node = null
+	 *  @param  string  $constructor
 	 *  @return object  Html5Search
 	 *  @access public
 	 */
-	public function find($construct, $node = null)
+	public function find($constructor)
 	{
-		//  the top level object node must be reset
-		$this->objnode = null;
-		
-		//  create an empty array placeholder list
-		$list = array();
-		
 		//  create an instance of the HTML5Contructor object
-		$this->construct = HTML5Construct::Explode($construct, true);
+		$construct = Html5Construct::Explode($constructor, true);
 		
 		//  define the parent top level object node to search against
 		$this->objnode = $this->parent->objnode;
 		
 		//  first, search for a match by id and return a result
-		if ($this->construct->id) {
-			if ($node = $this->domobj->getElementById($this->construct->id)) {
+		if ($construct->id) {
+			if ($node = $this->domobj->getElementById($construct->id)) {
 				//  the found node becomes the top level object node
 				$this->list = array($node);
 			}
@@ -136,25 +128,25 @@ class Html5Search extends Html5
 		}
 		
 		//  search for a match by nodeName, if provided
-		if ($this->construct->name) {
+		if ($construct->name) {
 			//  create a local copy of the list
-			$list = (count($this->list)) ? $this->list : $list = $this->objnode->childNodes;
+			$list = (count($this->list)) ? $this->list : $this->objnode->childNodes;
 			//  reset the instance list to rebuild with matches
 			$this->list = array();
 			
 			//  cycle the list and strike a match
-			foreach ($list as $each) { $this->findByNodeName($this->construct->name, $each); }
+			foreach ($list as $each) { $this->findByNodeName($construct->name, $each); }
 		}
 		
 		//  search for a match by className, if provided
-		if ($this->construct->class) {
+		if ($construct->class) {
 			//  create a local copy of the list
 			$list = (count($this->list)) ? $this->list : $this->parent->body->childNodes;
 			//  reset the instance list to rebuild with matches
 			$this->list = array();
 			
 			//  cycle the list and strike a match
-			foreach ($list as $each) { $this->findbyClassName($this->construct->class, $each); }
+			foreach ($list as $each) { $this->findByClassName($construct->class, $each); }
 		}
 		
 		//  count the number of matching nodes
@@ -173,6 +165,60 @@ class Html5Search extends Html5
 	public function item($index = 0)
 	{
 		$this->objnode = ($index < $this->length) ? $this->list[$index] : null;
+		
+		return $this;
+	}
+	
+	/**
+	 *  not()
+	 *  Exclude matching nodes against a provided construct
+	 *  
+	 *  @param  string  $constructor
+	 *  @return object  Html5Search
+	 *  @access public
+	 */
+	public function not($constructor)
+	{
+		//  exit if there isn't a list to exclude from
+		if (!count($this->list)) { return $this; }
+		
+		//  create an instance of the HTML5Contructor object
+		$construct = Html5Construct::Explode($constructor, true);
+		
+		//  create a local empty list
+		$list = array();
+		
+		//  cycle the instance list and determine whether to exclude
+		foreach ($this->list as $each) {
+			$exclude = false;
+			
+			//  examine a match by id and exclude if necessary
+			if ($construct->id && $each->hasAttribute("id") && ($each->getAttribute("id") == $construct->id)) {
+				$exclude = true;
+			}
+			
+			//  examine a match by nodeName and exclude if necessary
+			if ($construct->name && ($each->nodeName == $construct->name)) {
+				$exclude = true;
+			}
+			
+			//  examine a match by className and exclude if necessary
+			if ($construct->class && $each->hasAttribute("class")) {
+				$match = explode(" ", $construct->class);
+				$class = explode(" ", $each->getAttribute("class"));
+				
+				if (array_intersect($match, $class) == $match) {
+					$exclude = true;
+				}
+			}
+			
+			//  append this node back into the list, if allowed
+			if (!$exclude) { $list[] = $each; }
+		}
+		
+		//  restore the local revised list into the instance
+		$this->list		= $list;
+		$this->length	= count($list);
 		
 		return $this;
 	}
