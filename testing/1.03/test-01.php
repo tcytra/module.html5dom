2837984ab5480f8245e96e5e326655a15efc54ce
Todd Cytra <tcytra@tninetwork.com>
2016-11-15 10:30:24 -0800

    File Update: Created the testing/1.03 directory index and test-01 script

---
+++ testing/1.03/test-01.php
@@ -0,0 +1,17 @@
+<?php ini_set("display_errors","on"); error_reporting(E_ALL); ?>
+<?php require "../../load.html5dom.php"; ?>
+<?php
+
+//  Title and describe this testcase
+$testcase = "1.03 | Test 01";
+$describe = "Require the ability to load a file into the Html5Document or Html5Fragment";
+
+//  Create an instance of the Html5Document
+$html5 = new Html5Document();
+
+//  Append some elements to the document <head>
+$html5->head->title($testcase)->meta( ["name"=>"description", "content"=>"{$testcase}: {$describe}"] );
+
+
+
+$html5->save()->write();
