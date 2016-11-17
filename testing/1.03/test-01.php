2b03fe0252eb082b330875093b22a47865f58e3c
Todd Cytra <tcytra@gmail.com>
2016-11-16 20:20:05 -0800

    Testing: 1.03-01 passed; Able to read html file content into document

--- testing/1.03/test-01.php
+++ testing/1.03/test-01.php
@@ -3,8 +3,9 @@
 <?php
 
 //  Title and describe this testcase
-$testcase = "1.03 | Test 01";
-$describe = "Require the ability to load a file into the Html5Document or Html5Fragment";
+$testcase = "Html5Dom 1.03; Test 01";
+$describe = "Require the ability to load a file into the Html5Document";
+$rootpath = dirname(__FILE__);
 
 //  Create an instance of the Html5Document
 $html5 = new Html5Document();
@@ -12,6 +13,7 @@ $html5 = new Html5Document();
 //  Append some elements to the document <head>
 $html5->head->title($testcase)->meta( ["name"=>"description", "content"=>"{$testcase}: {$describe}"] );
 
-
+//  Load the target html file into the document body
+$html5->loadFile( "{$rootpath}/index.html" );
 
 $html5->save()->write();
